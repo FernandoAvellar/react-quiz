e2a46e89a72de6d95f3fd9493cc5338c11b4dcda
Fernando Avellar Jr. <favellar@yahoo.com.br>
2024-09-17 14:42:31 -0300

    Add json-server and initial data fetch logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import DateCounter from './components/DateCounter';
+import { useEffect, useReducer } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action is unknown');
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log(state);
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const resp = await fetch('http://localhost:8000/questions');
+        const data = await resp.json();
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (error) {
+        dispatch({ type: 'dataFailed' });
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
